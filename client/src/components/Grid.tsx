139328dab4c1376c219fd0fe0d1e2cf2c1d7d661
markbakos <markbakosss@gmail.com>
2025-01-04 17:01:38 +0100

    Added flagging

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -2,10 +2,11 @@ import { Cell } from "../types"
 import {useEffect, useState} from "react";
 
 const Grid: React.FC = () => {
-    const size = 10
-    const bombCount = 15
+    const size = 20
+    const bombCount = 50
     const [grid, setGrid] = useState<Cell[][]>([])
-    const [isGameOver, setIsGameOver] = useState(false);
+    const [isGameOver, setIsGameOver] = useState(false)
+    const [isFlagging, setIsFlagging] = useState(false);
 
     useEffect(() => {
         initializeGrid()
@@ -16,6 +17,7 @@ const Grid: React.FC = () => {
             Array.from({ length: size }, () => ({
                 revealed: false,
                 value: 0,
+                flagged: false,
             }))
         )
 
@@ -96,39 +98,103 @@ const Grid: React.FC = () => {
     }
 
     const handleClick = (row: number, col: number) => {
-        if(!isGameOver){
+        if(isGameOver) return
+
+        if(isFlagging) {
+            setGrid((prev) => {
+                const newGrid = prev.map((row) =>
+                    row.map((cell) => ({...cell}))
+                )
+                newGrid[row][col].flagged = !newGrid[row][col].flagged
+                return newGrid
+            })
+        } else {
             setGrid((prev) => {
                 const newGrid: Cell[][] = prev.map(row => row.map(cell => ({...cell})))
+                if (newGrid[row][col].flagged) return newGrid;
 
                 if (newGrid[row][col].value === "bomb") {
                     newGrid[row][col].revealed = true
                     setIsGameOver(true)
                     return newGrid
                 }
-
                 return revealCell(newGrid, row, col)
             })
         }
+    }
 
+    const handleRightClick = (e: React.MouseEvent, row: number, col: number)=> {
+        e.preventDefault()
+        if(isGameOver) return
+
+        setGrid((prev) => {
+            const newGrid = prev.map((row) =>
+                row.map((cell) => ({...cell}))
+            )
+            newGrid[row][col].flagged = !newGrid[row][col].flagged
+            return newGrid
+        })
+    }
+
+    const restartGame = () => {
+        initializeGrid()
+        setIsGameOver(false)
+    }
+
+    const getNumberColor = (value: number | "bomb") => {
+        switch (value) {
+            case 1:
+                return "text-blue-700"
+            case 2:
+                return "text-green-700"
+            case 3:
+                return "text-red-500"
+            case 4:
+                return "text-blue-900"
+            case 5:
+                return "text-red-800"
+            case 6:
+                return "text-cyan-700"
+            case 7:
+                return "text-purple-700"
+            case 8:
+                return "text-gray-600"
+            default:
+                return ""
+        }
     }
 
     return (
-        <div className="inline-block">
-            {grid.map((row, rowIndex) =>
-                <div key={rowIndex} className="flex">
-                    {row.map((cell, colIndex) => (
-                        <div
-                            key={`${rowIndex}-${colIndex}`}
-                            className={`w-8 h-8 border border-gray-500 bg-gray-200  ${cell.revealed ? cell.value === "bomb" ? "bg-red-500" : "bg-gray-400" : "bg-gray-200 hover:bg-gray-300"}`}
-                            onClick={() => handleClick(rowIndex, colIndex)}
-                        >
-                            {cell.revealed && cell.value === "bomb" && <p className="select-none text-center text-lg font-semibold">💣</p>}
-                            {cell.revealed && cell.value !== 0 && cell.value !== "bomb" && <p className="select-none text-center text-lg font-semibold">{cell.value}</p>}
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+        <>
+            <button onClick={() => setIsFlagging(!isFlagging)} className="border-gray-600 border bg-gray-500 w-56 h-8 text-white text-lg">
+                {!isFlagging ? "Enable Flagging" : "Disable Flagging"}
+            </button>
+            <div className="inline-block">
+                {grid.map((row, rowIndex) =>
+                    <div key={rowIndex} className="flex">
+                        {row.map((cell, colIndex) => (
+                            <div
+                                key={`${rowIndex}-${colIndex}`}
+                                className={`w-8 h-8 border border-gray-500 bg-gray-200  ${cell.revealed ? cell.value === "bomb" ? "bg-red-500" : "bg-gray-400" : "bg-gray-200 hover:bg-gray-300"}`}
+                                onClick={() => handleClick(rowIndex, colIndex)}
+                                onContextMenu={(e) => handleRightClick(e, rowIndex, colIndex)}
+                            >
+                                {cell.revealed && cell.value === "bomb" &&
+                                    <p className="select-none text-center text-lg font-semibold">💣</p>}
+                                {cell.revealed && cell.value !== 0 && cell.value !== "bomb" &&
+                                    <p className={`select-none text-center text-2xl font-bold ${getNumberColor(cell.value)}`}>{cell.value}</p>}
+                                {cell.flagged && !cell.revealed &&
+                                    <p className="select-none text-center text-lg font-semibold">🚩</p>}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            <button onClick={restartGame} className="border-gray-600 border bg-gray-500 w-32 h-8 text-white text-xl">New
+                Game
+            </button>
+        </>
+
     )
 }
 
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,4 +1,5 @@
 export type Cell = {
     revealed: boolean
     value: number | 'bomb'
+    flagged: boolean
 }
