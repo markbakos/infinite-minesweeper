15c01407f5cb6d24d8652f8d9ec49f0593fd2361
markbakos <markbakosss@gmail.com>
2025-03-10 23:14:57 +0100

    added Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Game} from "./pages/Game.tsx";
 import {Home} from "./pages/Home.tsx";
 import {SignUp} from "./pages/SignUp.tsx";
+import {Login} from "./pages/Login.tsx";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/infinite" element={<Game game="infinite" />} />
                     <Route path="/normal" element={<Game game="normal" />} />
                     <Route path="/signup" element={<SignUp />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </div>
         </Router>
